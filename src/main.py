3fc23b85feebc239b75d0e6b6e77ba724e2d334b
Denis Ivanov <dnb8866@gmail.com>
2024-12-07 14:42:36 +0700

    raise http 400 if data not valid, try/except for db

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from utils.service import check_fields
 from utils.db import collection
@@ -7,10 +7,25 @@ app = FastAPI()
 
 
 @app.post('/get_form')
-async def get_form(data: dict):
-    if valid_data := check_fields(data):
+async def get_form(data: dict[str, str]):
+    """
+    Получение имени формы по переданным данным, если такая форма существует,
+    иначе возвращает типы полей в форме.
+    """
+    valid_data = check_fields(data)
+    if not valid_data:
+        raise HTTPException(
+            status_code=400,
+            detail='Invalid data'
+        )
+    try:
         cursor = collection.find(valid_data)
         forms = await cursor.to_list(length=None)
-        if forms and 'form_name' in forms[0]:
-            return {'form_name': forms[0]['form_name']}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database error: {str(e)}"
+        )
+    if forms and 'form_name' in forms[0]:
+        return {'form_name': forms[0]['form_name']}
     return valid_data
